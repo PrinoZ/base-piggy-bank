771704f2464d88fdd1872574be43a10aef46fc81
PrinoZ <zhiyuanzuo3@gmail.com>
2025-12-11 06:41:24 +0000

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // ...保持你原有的其他配置
+  webpack: (config) => {
+    // 忽略这些在客户端构建中不需要的库
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
